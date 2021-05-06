173958aa3c754ca418da99a98a5caa628de47229
Lukasz <srednickilukasz@gmail.com>
2021-05-06 11:25:10 +0200

    Conditional styling

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 let outputValue = 0;
+const counter = document.querySelector('.counter');
 
 // counter
 document.querySelectorAll('.btn').forEach(btn => {
     btn.addEventListener('click', e => {
         const btnClassList = e.currentTarget.classList;
 
-        btnClassList.contains('plus') ? outputValue++ : btnClassList.contains('minus') ? outputValue-- : outputValue = 0;
+        btnClassList.contains('plus') ? outputValue++
+        : btnClassList.contains('minus') ? outputValue--
+        : outputValue = 0;
 
-        document.querySelector('.counter').textContent = outputValue;
+        outputValue > 0 ? counter.style.color = 'green'
+        : outputValue < 0 ? counter.style.color = 'red'
+        : counter.style.color = 'black'
+
+        counter.textContent = outputValue;
     })
 });
